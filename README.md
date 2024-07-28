4544298df41fcab395d8732391c5ffcc34da73ae
Kshitij Rajgude <kshitijrajgude@gmail.com>
2024-07-28 23:26:43 +0530

    fix: render glitch

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # A Gamish Portfolio
 A Mario Inspired Web Portfolio. 
 Using Vanilla Javascript, little html and a bit of css, All it takes to create a simple 2d portfolio just like mario.
+
+## Todo list
+- [ ] Learn HTML canvas
+- [ ] create platform and character
+- [ ] add gravity
+- [ ] add keystroke moments
+
--- main.js
+++ main.js
@@ -2,10 +2,8 @@
 const canvas = document.getElementById("platformer");
 const ctx = canvas.getContext("2d");
 
-// Getting the keyboard keystrokes
-document.addEventListener("keydown", (event) => {
-    console.log(event.key);
-})
+
+let g = 2;
 
 
 class Platformer{
@@ -25,12 +23,62 @@ class Player{
         ctx.fillStyle = "black";
         ctx.fillRect(this.px, this.py, this.height, this.width);
     }
-    moveRight(){}
-    moveLeft(){}
+    clear(){
+        ctx.clearRect(this.px - 5, this.py - 5, this.height + 10, this.width + 10);
+    }
+    gravity(){
+        // gravity
+        if(this.py + this.height + 20 < document.getElementById("platformer").height){
+            this.py += g - this.vy;
+            this.vy -= g;
+        } else {
+        }
+        // Momentum
+        if(this.vx != 0){
+            if(this.vx > 0){ // left
+                this.px += this.vx + 0.5;
+                this.vx -= 0.5;
+            } else { // right
+                this.px += this.vx - 0.5;
+                this.vx += 0.5;
+            }
+        }
+    }
+    jump(){
+        this.py -= this.vy;
+    }
+    moveHorizontal(){
+        this.px += vx;
+    }
     dead(){}
-    jump(){}
-
 }
 
 let p1 = new Player;
-p1.draw();
+// Getting the keyboard keystrokes
+document.addEventListener("keydown", (event) => {
+    console.log(event.key == 'w');
+    lastKeyStroke = event.key;
+    if(event.key=='w'){
+        p1.vy = 10;
+        p1.clear();
+        p1.jump();
+    }
+    if(event.key=="a"){
+        console.log("left");
+        p1.vx = -5;
+        p1.clear();
+        p1.moveHorizontal();
+    }
+    if(event.key=="d"){
+        console.log("right")
+        p1.vx = 5;
+        p1.clear();
+        p1.moveHorizontal();
+    }
+})
+
+setInterval(() => {
+    p1.clear();
+    p1.gravity();
+    p1.draw();
+}, 30);
